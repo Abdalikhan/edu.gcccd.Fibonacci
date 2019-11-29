03ef6fa3c24c26c0771b80a45deeec48bfe19bec
Abdalikhan <52061303+Abdalikhan@users.noreply.github.com>
2019-11-28 22:01:46 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -10,24 +10,35 @@ public class Main {
 		            return b; 
 		        return fib(n - 1, b, a.add(b)); 
 		    } 
-		      
+		      static int sum_iter(int n) {
+		    	  int sum = 0;
+		    	  for (int i =0 ; i <= n; i++) {
+		    		  sum+=i;
+		    	  }
+		    	  return sum;
+		      }
 		    public static void main (String[] args)  
 		    { 
+		    	int n =2000;
+		    	int k = n*(n+1)/2;
+		    	//System.out.println(sum(n));
+		    	System.out.println(k);
+		    	System.out.println(sum_iter(n));
 		        long start = System.currentTimeMillis();
-		        int n = Integer.parseInt(args[0]);
+		       // int n = Integer.parseInt(args[0]);
 		        //int n =
 		        //int n = 9;
 		        BigInteger zero = BigInteger.valueOf(0);
 		        BigInteger one = BigInteger.valueOf(1);
 		        BigInteger result = fib(n,zero,one);
 		        long finish = System.currentTimeMillis();
-		        long time = finish - start;
+		        long time = finish - start;		       
 		        System.out.println("The " + n + "-th Fibonacci Number is " + result + " and was calculated in " + time + " ms.");  
 		        System.out.println("The fib() method got called " + numOfCalled + " times.");
 		    } 
 		}
 
-			
+		
 		
 
 
